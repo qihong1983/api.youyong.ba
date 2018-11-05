3c5388ddb416848c0130f5fcab423495f6261fbc
qihong1 <qihong1@lenovo.com>
2018-11-05 23:28:24 +0800

    asdfasf

--- routes/textToVideo/textToVideo.js
+++ routes/textToVideo/textToVideo.js
@@ -21,8 +21,6 @@ var urlencode = require('urlencode');
 
 
 
-var apiSecretKey = '1a3c1500ab253b636100c053e1ebea87';
-
 var options = {
 	"auf": "audio/L16;rate=16000",
 	"aue": "raw",
@@ -30,21 +28,16 @@ var options = {
 	"speed": "50",
 	"volume": "50",
 	"pitch": "50",
-	"engine_type": "intp65",
+	"engine_type": "sms16k",
 	"text_type": "text"
 };
 
-
-// console.log(JSON.stringify(options));
-
-
-// var xParam = JSON.stringify(options).toString('base64');
 
 var currentText = '科大讯飞的接口文档写的太烂了';
 var APPID = '5bbf9b42';
 var API_KEY = '5af95771bb7c1574f1ccc532871c530a';
 var xParam = new Buffer(JSON.stringify(options)).toString('base64')
-console.log(xParam, 'xParam');
+
 
 var xCurTime = Math.floor((new Date()).valueOf() / 1000);
 var md5 = crypto.createHash("md5");
@@ -52,10 +45,6 @@ var md5 = crypto.createHash("md5");
 var xCheckSum = md5.update(API_KEY + xCurTime + xParam).digest("hex");
 
 
-
-console.log(API_KEY, '#############', xCurTime);
-
-console.log(xCheckSum, '111');
 
 var headers = {
 	"X-Appid": APPID,
@@ -83,32 +72,3 @@ axios({
 }).catch(err => {
 	console.log(err)
 })
-// JSON.stringify(options.toString('base64'));
-
-// var req = http.request(options, function(res) {
-// 	console.log('STATUS: ' + res.statusCode);
-// 	console.log('HEADERS: ' + JSON.stringify(res.headers));
-// 	res.setEncoding('utf8');
-// 	res.on('data', function(chunk) {
-// 		console.log('BODY: ' + chunk);
-// 	});
-// });
-
-// req.on('error', function(e) {
-// 	console.log('problem with request: ' + e.message);
-// });
-
-// // write data to request body  
-// req.write(content);
-
-// req.end();
-
-// console.log(options)
-
-// router.get('/', bodyParser.json(), function(req, res, next) {
-
-
-
-// });
-
-// module.exports = router;
