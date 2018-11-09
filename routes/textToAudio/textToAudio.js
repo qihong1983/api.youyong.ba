fc99d44e01a9392899a2e4f1d77eae0ef69cabe4
qihong1 <qihong1@lenovo.com>
2018-11-10 03:01:42 +0800

    asdfa

--- routes/textToAudio/textToAudio.js
+++ routes/textToAudio/textToAudio.js
@@ -19,13 +19,105 @@ const xfApiKey = "5af95771bb7c1574f1ccc532871c530a";
 
 
 
-router.get('/', bodyParser.json(), function(req, res, next) {
-	// tts(res);
-	res.json({
-		status: false,
-		msg: "执行失败",
-		data: req.query
+router.use(async function(req, res, next) {
+
+	console.log(req.query);
+	var jsonOri = {
+		"auf": "audio/L16;rate=16000",
+		"aue": "raw",
+		"voice_name": "xiaoyan",
+	};
+	var param = Buffer.from(JSON.stringify(jsonOri)).toString('base64');
+	console.log(param)
+	var curTime = Date.parse(new Date()).toString().slice(0, -3);
+	var checkSum = crypto.createHash('md5').update(xfApiKey + curTime + param).digest('hex');
+	let sendtext = {
+		'text': '的过程。每个周期，投诉都围绕进展。整个团队正致力于改进流程，但我觉得仍有工作要做。老实说，谷歌不再是一个小的初创公司，在一个8万人的大公司中寻找平衡很难。这让我想到了 Google Cloud。'
+	}
+
+	var audio = '';
+	var options = {
+		url: xfUrl,
+		method: "POST",
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+			"X-Param": param,
+			"X-Appid": xfAppid,
+			"X-CurTime": curTime,
+			"X-CheckSum": checkSum,
+		},
+		form: {
+			text: req.query.text
+		},
+		encoding: null
+	}
+
+	await request(options, async function(error, response, body) {
+
+
+		if (!error && response.statusCode == 200) {
+
+			console.log(4444444444);
+			console.log(req.query.text, '44444444444444444444');
+
+			let headers = response.headers;
+
+			console.log(headers["content-type"]);
+			// if (headers["content-type"] == "audio/mpeg") {
+			console.log("成功");
+
+			await fs.writeFile("../../public/audio.wav", body, async function(err) {
+
+				console.log(body, '#####');
+
+				if (err) {
+					return console.error(err);
+
+					await res.json({
+						status: false,
+						msg: "执行失败",
+						data: err
+					})
+					// return false;
+				} else {
+					await res.json({
+						status: true,
+						msg: "执行成功",
+						data: 'https://api.youyong.ba/audio.wav',
+						aaa: '11'
+					})
+				}
+			})
+			// } else {
+			// 	console.log(123);
+			// 	console.log(response.body);
+			// }
+		} else {
+			console.log('333333333333333333333333');
+			console.log(error);
+		}
 	});
+});
+
+
+router.get('/', bodyParser.json(), function(req, res, next) {
+
+	// tts(res);
+	// res.json({
+	// 	status: false,
+	// 	msg: "执行失败",
+	// 	data: req.query
+	// })
+
+	next();
+
+	// tts(res);
+	// res.json({
+	// 	status: false,
+	// 	msg: "执行失败",
+	// 	data: req.query
+	// })
+
 })
 
 
