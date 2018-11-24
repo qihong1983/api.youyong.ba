15b19a21cf2a63fd31806ba1d21fec945d950aba
qihong1 <qihong1@lenovo.com>
2018-11-24 19:19:18 +0800

    asdf

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ var updateUuid = require('./routes/updateUuid');
 //查询 queryUuid 
 var queryUuid = require('./routes/queryUuid');
 
+
+var cname = require('./routes/cname');
+
 //文字转语音
 
 var textToAudio = require('./routes/textToAudio/textToAudio');
@@ -71,8 +74,10 @@ app.use('/queryUuid', queryUuid);
 app.use('/textToAudio', textToAudio);
 
 
+app.use('/cname', cname);
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
 
   console.log(req, res, next);
   var err = new Error('Not Found');
@@ -85,7 +90,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -96,7 +101,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
